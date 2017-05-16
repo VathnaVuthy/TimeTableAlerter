4c7b3a706914432f696af05cfd832c24217c4028
Sokchanbo <chanbo97@gmail.com>
2017-05-16 19:26:21 +0700

    Add settingActivity

---
+++ app/src/main/java/com/supperapper/timetablealerter/activity/SettingActivity.java
@@ -0,0 +1,26 @@
+package com.supperapper.timetablealerter.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.supperapper.timetablealerter.R;
+
+/**
+ * Created by USER on 5/16/2017.
+ */
+
+public class SettingActivity extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.setting_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Setting");
+
+
+    }
+}
