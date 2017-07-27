a1d3201bb4b7a71a13650355a3cb2ad6bd1be4dd
Vathna <vuthyvathna123@gmail.com>
2017-07-27 14:25:03 +0700

    -Add Singleton

--- app/src/main/java/com/supperapper/timetablealerter/activity/LoginActivity.java
+++ app/src/main/java/com/supperapper/timetablealerter/activity/LoginActivity.java
@@ -2,8 +2,12 @@ package com.supperapper.timetablealerter.activity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +25,7 @@ import com.supperapper.timetablealerter.R;
 import com.supperapper.timetablealerter.dataset.App;
 
 import java.lang.reflect.Array;
+import java.security.MessageDigest;
 import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
@@ -101,7 +106,26 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+    }
 
+    private String getHashKey() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+             for (Signature signature : info.signatures) {
+                MessageDigest messageDigest = MessageDigest.getInstance("SHA");
+                messageDigest.update(signature.toByteArray());
+                String hashKey = new String(Base64.encode(messageDigest.digest(), 0));
+                    if (!hashKey.isEmpty()) {
+                     return hashKey;
+                }
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+         return "";
     }
 
     @Override
